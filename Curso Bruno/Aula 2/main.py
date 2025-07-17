d45a7d7491d37db50d059c66117587e9e4671e8b
guibardev <guibardevs@gmail.com>
2025-07-16 21:59:44 -0300

    projeto ok

---
+++ Curso Bruno/Aula 2/main.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+pyautogui.alert('Olá! Isso é um teste com PyAutoGUI.')
+
+# pyautogui.hotkey('alt' , 'tab', 'esc')
+# time.sleep(300/1000)
+# pyautogui.write('QQQQ', interval=0.25)
+
+#pyautogui.hotkey('alt', 'tab')
+#time.sleep(3000 / 1000)
+#pyautogui.hotkey('esc')
+#time.sleep(3000 / 1000)
+#pyautogui.hotkey('Q')
+
